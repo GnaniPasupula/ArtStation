ac0aa93f37d858e5b146be755b1bbbcebfe2dc3a
GnaniPasupula <gnanipasupula@gmail.com>
2022-05-11 18:31:04 +0530

    Fixed Router Firebase Errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/Profile.dart';
-import 'package:portfolio/Search.dart';
 import 'package:portfolio/wrapper.dart';
-import 'Work.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,82 +10,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Wrapper(),
-    );
-  }
-}
-
-Widget customBottomNavigationBar(BuildContext context) {
-  return Container(
-    decoration:
-        BoxDecoration(border: Border.all(color: Colors.black, width: 2)),
-    height: 100,
-    width: MediaQuery.of(context).size.width,
-    child: const TabBar(
-      tabs: [
-        Tab(
-            icon: Icon(
-          Icons.home_outlined,
-          size: 30.0,
-          color: Colors.black,
-        )),
-        Tab(
-            icon: Icon(
-          Icons.search_outlined,
-          size: 30.0,
-          color: Colors.black,
-        )),
-        Tab(
-            icon: Icon(
-          Icons.add_circle_outline_outlined,
-          size: 50.0,
-          color: Colors.black,
-        )),
-        Tab(
-            icon: Icon(
-          Icons.favorite_outline,
-          size: 30.0,
-          color: Colors.black,
-        )),
-        Tab(
-            icon: Icon(
-          Icons.person_outline,
-          size: 30.0,
-          color: Colors.black,
-        )),
-      ],
-      indicatorSize: TabBarIndicatorSize.label,
-      indicatorColor: Colors.white,
-    ),
-  );
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: DefaultTabController(
-        length: 5,
-        child: Scaffold(
-          body: TabBarView(
-            children: [
-              Work(),
-              Search(),
-              Work(),
-              Work(),
-              Profile(),
-            ],
-          ),
-          bottomNavigationBar: customBottomNavigationBar(context),
-          backgroundColor: const Color.fromARGB(255, 253, 199, 49),
-        ),
-      ),
     );
   }
 }
